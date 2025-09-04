1c6e8954664a029d02686937c2ce74a1e7b4c253
ankit-paul-keross <ankit.paul@keross.com>
2025-09-04 13:37:32 +0530

    added theme toggle button

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-import HelloWorld from "@/registry/new-york/helloWorld";
+
 
 export default function Home() {
   return (
     <div className="p-5">
-      <div className="my-5">Shadcn Install link: npx shadcn@latest add https://custom-components-sable.vercel.app/r/hello-world.json</div>
 
-      <HelloWorld/>
+   
     
     </div>
   );
