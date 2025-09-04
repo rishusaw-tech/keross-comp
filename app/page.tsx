ae0d4ea8a5f00d2a0af299875cedd5932920c3ac
ankit-paul-keross <ankit.paul@keross.com>
2025-09-04 11:59:14 +0530

    add custom component

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,11 @@ import HelloWorld from "@/registry/new-york/helloWorld";
 
 export default function Home() {
   return (
-    <HelloWorld/>
+    <div className="p-5">
+      <div className="my-5">Shadcn Install link: npx shadcn@latest add https://custom-components-sable.vercel.app/r/hello-world.json</div>
+
+      <HelloWorld/>
+    
+    </div>
   );
 }
