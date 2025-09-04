fdd80c9c8f24eb3920f2f084c8331189ade20a4c
ankit-paul-keross <ankit.paul@keross.com>
2025-09-04 15:26:39 +0530

    added alert

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
+import { Alert, AlertDescription, AlertTitle } from "@/registry/new-york/alert";
+import { CheckCircle2Icon } from "lucide-react";
 
 
 export default function Home() {
   return (
-    <div className="p-5">
-
-   
-    
+    <div className="m-5 flex flex-col">
+      <div className="mb-2">Alert</div>
+      <Alert className="w-2xl mb-5">
+        <CheckCircle2Icon />
+        <AlertTitle>Success! Your changes have been saved</AlertTitle>
+        <AlertDescription>
+          This is an alert with icon, title and description.
+        </AlertDescription>
+      </Alert>    
     </div>
   );
 }
